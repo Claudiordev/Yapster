6194314b00da02bc6c469c53702830667ba34627
Claudiordev <claudiordev@gmail.com>
2026-05-10 00:58:01 +0200

    feat: implement HTTP problem details response utils

--- common-utils/src/main/java/com/claudiordese/exceptions/GlobalExceptionControllerAdvice.java
+++ common-utils/src/main/java/com/claudiordese/exceptions/GlobalExceptionControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.claudiordese.exceptions;
 
+import com.claudiordese.utils.ProblemDetails;
 import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
@@ -9,9 +10,7 @@ import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.net.URI;
 import java.util.Map;
 
 @ControllerAdvice
@@ -26,8 +25,8 @@ public class GlobalExceptionControllerAdvice {
     );
 
     @ExceptionHandler(GlobalException.class)
-    public ResponseEntity<?> handleLibraryException(GlobalException e, HttpServletRequest request) {
-        logger.warn("Error: {}, details: {}",e.getCode(),e.getMessage());
+    public ResponseEntity<ProblemDetail> handleLibraryException(GlobalException e, HttpServletRequest request) {
+        logger.warn("Error: {}, details: {}", e.getCode(), e.getMessage());
         HttpStatus httpStatus = STATUS_MAPPING.entrySet()
                 .stream()
                 .filter(y -> y.getKey().isAssignableFrom(e.getClass()))
@@ -35,15 +34,8 @@ public class GlobalExceptionControllerAdvice {
                 .findFirst()
                 .orElse(HttpStatus.INTERNAL_SERVER_ERROR);
 
-        var problemDetail = ProblemDetail.forStatus(httpStatus);
-        problemDetail.setTitle(e.getCode());
-        problemDetail.setDetail(e.getMessage());
-        problemDetail.setType(URI.create("http://localhost:8080/error/" + e.getCode()));
-        problemDetail.setProperty("code", e.getCode());
-        problemDetail.setProperty("message", e.getMessage());
-        problemDetail.setInstance(URI.create(request.getRequestURI()));
-
-        return ResponseEntity.status(httpStatus).body(problemDetail);
+        ProblemDetail problem = ProblemDetails.of(httpStatus, e.getCode(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(httpStatus).body(problem);
     }
 
     @PostConstruct
