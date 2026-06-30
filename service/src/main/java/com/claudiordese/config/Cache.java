6c576e836eb95a2e4afe215ba786d09345f96541
Claudio Rebelo <claudiordev@gmail.com>
2026-06-30 14:20:58 +0100

    chore: remove unused modules from repo and build (#14)
    - Remove unused modules

--- service/src/main/java/com/claudiordese/config/Cache.java
+++
@@ -1,9 +0,0 @@
-package com.claudiordese.config;
-
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableCaching
-public class Cache {
-}
